1017364c4ffda2373e281d8f28363657bae918d8
Andrea Draghetti <drego85@draghetti.it>
2015-12-26 18:59:00 +0100

    Update Title

--- hack/rce.php
+++ hack/rce.php
@@ -11,7 +11,7 @@ auth();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>quasiBot | rce</title>
+    <title>quasiBot | RCE</title>
     <link href="../style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
