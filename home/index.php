0880c44bfbebf4ec6de46cebed3b023d1e545e21
Andrea Draghetti <drego85@draghetti.it>
2015-12-26 18:46:18 +0100

    Update Header

--- home/index.php
+++ home/index.php
@@ -20,7 +20,7 @@ auth();
 
 
         <div id="logo">
-            <h1><a href="index.php">&nbsp; &nbsp; &nbsp;<?php echo date('H:i:s') ?></a></h1>
+            <h1><a href="index.php">&nbsp; &nbsp; &nbsp;<?php echo "QuasiBot" ?></a></h1>
 
 <?php
 quotes();
@@ -33,7 +33,7 @@ quotes();
             <li class="txt_left"><?php echo $_SERVER['DOCUMENT_ROOT'] ?></li>
             <li class="txt_left"><?php echo $_SERVER['PHP_SELF'] ?></li>
             <li class="txt_center"><?php //echo date("d.m.y"); ?></li>
-            <li class="txt_right"><?php echo $_SERVER['SERVER_ADDR'] ?></li>
+            <li class="txt_right"><?php //echo $_SERVER['SERVER_ADDR'] ?></li>
             <li class="txt_right"><?php echo $_SERVER['SERVER_NAME'] ?></li>
             <li class="txt_right"><?php echo conn('http://bot.whatismyipaddress.com/') ?></li>
         </ul>
